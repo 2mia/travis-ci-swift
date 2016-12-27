974e1210538c2fe19d350dfda8c210852f328cb7
Mihai Iancu <miancu@adobe.com>
2016-12-27 13:37:55 +0200

    Initial Commit

---
+++ hello-travisTests/hello_travisTests.swift
@@ -0,0 +1,36 @@
+//
+//  hello_travisTests.swift
+//  hello-travisTests
+//
+//  Created by Mihai Iancu on 27/12/2016.
+//  Copyright © 2016 MA. All rights reserved.
+//
+
+import XCTest
+@testable import hello_travis
+
+class hello_travisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
